0710ed34fc7ded771e60701c830afdebaf037297
uyilmazz <96124332+uyilmazz@users.noreply.github.com>
2021-12-31 21:19:23 +0300

    Update task7.sql

--- task7.sql
+++ task7.sql
@@ -1,15 +1,15 @@
 
 
 
--- SELECT rating, COUNT(*) FROM film
--- GROUP BY rating;
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating;
 
--- SELECT replacement_cost, COUNT(*) FROM film
--- GROUP BY replacement_cost
--- HAVING COUNT(*) > 50;
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
 
--- SELECT store_id, COUNT(*) FROM customer
--- GROUP BY store_id;
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
 
 SELECT country_id, COUNT(*) FROM city
 GROUP BY country_id
