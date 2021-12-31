ae5b3f6cc3e25e959d161a0d9bfb1d1a13f7ce5b
uyilmazz <96124332+uyilmazz@users.noreply.github.com>
2021-12-31 12:00:00 +0300

    Create task4.sql

---
+++ task4.sql
@@ -0,0 +1,16 @@
+
+
+SELECT DISTINCT(replacement_cost) FROM film;
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+SELECT COUNT(*) from film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+SELECT COUNT(*) FROM country
+WHERE LENGTH(country) = 5;
+
+SELECT COUNT(*) FROM city
+WHERE city ILIKE '%R';
+
+
