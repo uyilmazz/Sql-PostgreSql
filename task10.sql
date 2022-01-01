3d519bec6779a50e3d8deff4e46835aa28272102
uyilmazz <96124332+uyilmazz@users.noreply.github.com>
2022-01-01 19:48:28 +0300

    Create task10.sql

---
+++ task10.sql
@@ -0,0 +1,13 @@
+
+
+
+SELECT city, country FROM city
+INNER JOIN country ON city.city_id = country.country_id;
+
+SELECT payment_id, first_name, last_name FROM payment
+RIGHT JOIN customer ON payment.customer_id = customer.customer_id;
+
+SELECT rental_id, first_name, last_name FROM rental
+FULL JOIN customer ON rental.customer_id = customer.customer_id;
+
+
